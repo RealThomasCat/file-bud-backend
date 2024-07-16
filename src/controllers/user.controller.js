cef96f72101cc38c1c2000259d6034d1adcb9efe
Harsh Mishra <84076686+Harsh9425@users.noreply.github.com>
2024-07-17 00:44:26 +0530

    Fixed Logout

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -288,6 +288,7 @@ const logoutUser = asyncHandler(async (req, res) => {
     const options = {
         httpOnly: true,
         secure: true,
+        sameSite: "None",
     };
 
     // Clear cookies and send response
